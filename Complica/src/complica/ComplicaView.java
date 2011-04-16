74b7dfbd042d6be96fa0c38558590e779d7e58e0
jmulrey <jmulrey@umw.edu>
2011-04-15 21:16:16 -0400

    Losing player goes first

--- Complica/src/complica/ComplicaView.java
+++ Complica/src/complica/ComplicaView.java
@@ -236,6 +236,15 @@ public class ComplicaView extends FrameView {
         jPanel32.setBackground(color);
         gameBoard.resetBoardArray();
         winner = false;
+        numMoves = 0;
+        if(p1.getLosses() > p1.getWins()){
+            lblPlayerTurn.setText(p1.getName() + "'s Move");
+            lblPlayerTurn.setForeground(Color.RED);
+        }
+        else{
+            lblPlayerTurn.setText(p2.getName() + "'s Move");
+            lblPlayerTurn.setForeground(Color.BLUE);
+        }
     }
 
     @Action
